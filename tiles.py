8dd57cd5a0f5eec86113ae9af50933cdfe963b15
jzhou077 <97851897+jzhou077@users.noreply.github.com>
2023-05-28 15:59:20 -0400

    added transparency

--- tiles.py
+++ tiles.py
@@ -4,5 +4,10 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,size):
         super().__init__()
         self.image = pygame.Surface((size, size))
-        self.image.fill('gray')
+        self.image.set_colorkey(0)
         self.rect = self.image.get_rect(topleft = pos)
+
+class TerrainTile(Tile):
+    def __init__(self, pos, size, surface):
+        super().__init__(pos, size)
+        self.image.blit(surface, (0, 0))
